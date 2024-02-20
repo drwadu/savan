dc7143c468ee3492b2e595851a86923003ab5829
drwadu <dominik.rusovac@hotmail.com>
2024-02-20 18:07:48 +0100

    update.

--- src/nav/soe.rs
+++ src/nav/soe.rs
@@ -1,24 +1,94 @@
-use super::Navigator;
 use super::super::lex;
-use clingo::{SolverLiteral, Symbol, ToSymbol};
-use std::collections::{HashMap, HashSet};
-use crate::nav::utils::ToHashSet;
-use std::sync::Arc;
+use super::Navigator;
 use crate::nav::errors::NavigatorError;
+use crate::nav::utils::ToHashSet;
+use std::collections::{HashMap, HashSet};
 
 pub trait Collect {
     fn sieve(&mut self, target_atoms: &[String]) -> super::Result<()>;
+    fn sieve_verbose(&mut self, target_atoms: &[String]) -> super::Result<()>;
 }
 impl Collect for Navigator {
     fn sieve(&mut self, target_atoms: &[String]) -> super::Result<()> {
         let mut or = ":-".to_owned();
         target_atoms.iter().for_each(|atom| {
-                    or = format!("{or} not {atom},");
-                });
+            or = format!("{or} not {atom},");
+        });
 
         or = format!("{}.", &or[..or.len() - 1]);
         self.add_rule(or.clone())?;
-        
+
+        let mut i = 1;
+        let mut to_observe = target_atoms.to_vec().to_hashset();
+        let mut collection = vec![].to_hashset();
+
+        while !to_observe.is_empty() {
+            let target = to_observe
+                .iter()
+                .next()
+                .and_then(|a| self.expression_to_literal(a))
+                .ok_or(NavigatorError::None)?;
+
+            let ctl = self.ctl.take().ok_or(NavigatorError::NoControl)?;
+            let mut solve_handle = ctl.solve(clingo::SolveMode::YIELD, &[target])?;
+
+            if solve_handle
+                .get()
+                .map(|r| r == clingo::SolveResult::SATISFIABLE)?
+                == false
+            {
+                println!("info: cannot cover all target atoms");
+                println!("info: stopped search");
+                break;
+            }
+
+            #[allow(clippy::needless_collect)]
+            while let Ok(Some(model)) = solve_handle.model() {
+                if let Ok(atoms) = model.symbols(clingo::ShowType::SHOWN) {
+                    match atoms
+                        .iter()
+                        .map(|a| to_observe.remove(&a.to_string()))
+                        .collect::<Vec<_>>()
+                        .iter()
+                        .any(|v| *v)
+                    {
+                        true => {
+                            if collection.insert(atoms.clone()) {
+                                println!("solution {:?}: ", i);
+                                let atoms_strings = atoms.iter().map(|atom| atom.to_string());
+                                atoms_strings.clone().for_each(|atom| print!("{} ", atom));
+                                i += 1;
+                                println!();
+
+                                break;
+                            }
+                        }
+                        _ => {
+                            solve_handle.resume()?;
+                            continue;
+                        } // did not observe anything new
+                    }
+                }
+            }
+
+            let ctl = solve_handle
+                .close()
+                .map_err(|e| NavigatorError::Clingo(e))?;
+            self.ctl = Some(ctl);
+        }
+
+        self.remove_rule(or)
+    }
+
+    fn sieve_verbose(&mut self, target_atoms: &[String]) -> super::Result<()> {
+        let mut or = ":-".to_owned();
+        target_atoms.iter().for_each(|atom| {
+            or = format!("{or} not {atom},");
+        });
+
+        or = format!("{}.", &or[..or.len() - 1]);
+        self.add_rule(or.clone())?;
+
         let mut n = 0;
         let mut freq_table: HashMap<clingo::Symbol, usize> = HashMap::new();
         target_atoms.iter().for_each(|atom| {
@@ -33,9 +103,10 @@ impl Collect for Navigator {
 
         while !to_observe.is_empty() {
             let target = to_observe
-                    .iter()
-                    .next()
-                    .and_then(|a| self.expression_to_literal(a)).ok_or(NavigatorError::None)?;
+                .iter()
+                .next()
+                .and_then(|a| self.expression_to_literal(a))
+                .ok_or(NavigatorError::None)?;
 
             let ctl = self.ctl.take().ok_or(NavigatorError::NoControl)?;
             let mut solve_handle = ctl.solve(clingo::SolveMode::YIELD, &[target])?;
@@ -69,9 +140,7 @@ impl Collect for Navigator {
                                 });
 
                                 println!("solution {:?}: ", i);
-                                let atoms_strings = atoms.iter().map(|atom| {
-                                    atom.to_string()
-                                });
+                                let atoms_strings = atoms.iter().map(|atom| atom.to_string());
                                 atoms_strings.clone().for_each(|atom| print!("{} ", atom));
                                 i += 1;
                                 println!();
@@ -88,8 +157,8 @@ impl Collect for Navigator {
             }
 
             let ctl = solve_handle
-            .close()
-            .map_err(|e| NavigatorError::Clingo(e))?;
+                .close()
+                .map_err(|e| NavigatorError::Clingo(e))?;
             self.ctl = Some(ctl);
         }
 
@@ -108,11 +177,13 @@ impl Collect for Navigator {
         };
 
         println!("-");
-        let c = (i-1) as f64;
-        freq_table.iter().for_each(|(k,v)| println!("{:.2} {}", *v as f64 / c, k.to_string()));
+        let c = (i - 1) as f64;
+        freq_table
+            .iter()
+            .for_each(|(k, v)| println!("{:.2} {}", *v as f64 / c, k.to_string()));
         println!(
-        "{:?} {:?}",
-        freq_table.values().filter(|v| **v != 0).count() as f64 / n as f64,
+            "{:?} {:?}",
+            freq_table.values().filter(|v| **v != 0).count() as f64 / n as f64,
             r
         );
         println!("-");
@@ -128,18 +199,15 @@ fn entropy(lookup_table: &HashMap<clingo::Symbol, usize>, sample_size: f64) -> f
         .sum::<f64>()
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::nav::errors::NavigatorError;
-
     use super::super::errors::Result;
-    use super::super::lex;
     use super::*;
 
     #[test]
     fn soe_sieve() -> Result<()> {
         let mut nav = Navigator::new("a;b. c;d :- b. e.", vec!["0".to_string()])?;
-        nav.sieve(&["a".to_owned(),"b".to_owned()])
+        nav.sieve(&["a".to_owned(), "b".to_owned()])?;
+        nav.sieve_verbose(&["a".to_owned(), "b".to_owned()])
     }
 }
