9c5243fc4df503179614f5548f61adc4910423f4
drwadu <dominik.rusovac@hotmail.com>
2025-02-13 15:06:50 +0100

    add sieve_quiet.

--- src/nav/soe.rs
+++ src/nav/soe.rs
@@ -6,6 +6,7 @@ use std::collections::{HashMap, HashSet};
 
 pub trait Collect {
     fn sieve(&mut self, target_atoms: &[String]) -> super::Result<()>;
+    fn sieve_quiet(&mut self, target_atoms: &[String]) -> Option<String>;
     fn sieve_verbose(&mut self, target_atoms: &[String]) -> super::Result<()>;
     fn sieve_outf2(&mut self, target_atoms: &[String]) -> super::Result<Vec<String>>;
 }
@@ -79,6 +80,65 @@ impl Collect for Navigator {
         }
 
         self.remove_rule(or)
+    }
+
+    fn sieve_quiet(&mut self, target_atoms: &[String]) -> Option<String> {
+        let mut or = ":-".to_owned();
+        target_atoms.iter().for_each(|atom| {
+            or = format!("{or} not {atom},");
+        });
+
+        or = format!("{}.", &or[..or.len() - 1]);
+        self.add_rule(or.clone()).ok()?;
+
+        let mut to_observe = target_atoms.to_vec().to_hashset();
+
+        while !to_observe.is_empty() {
+            let (target_atom, target) = to_observe
+                .iter()
+                .next()
+                .map(|a| (a, self.expression_to_literal(a).unwrap()))?;
+
+            let ctl = self.ctl.take()?;
+            let mut solve_handle = ctl.solve(clingo::SolveMode::YIELD, &[target]).ok()?;
+
+            if solve_handle
+                .get()
+                .map(|r| r == clingo::SolveResult::SATISFIABLE)
+                .ok()?
+                == false
+            {
+                return Some(target_atom.to_string());
+            }
+
+            #[allow(clippy::needless_collect)]
+            while let Ok(Some(model)) = solve_handle.model() {
+                if let Ok(atoms) = model.symbols(clingo::ShowType::SHOWN) {
+                    match atoms
+                        .iter()
+                        .map(|a| to_observe.remove(&a.to_string()))
+                        .collect::<Vec<_>>()
+                        .iter()
+                        .any(|v| *v)
+                    {
+                        true => {
+                            break;
+                        }
+                        _ => {
+                            solve_handle.resume().ok()?;
+                            continue;
+                        } // did not observe anything new
+                    }
+                }
+            }
+
+            let ctl = solve_handle.close().ok()?;
+            self.ctl = Some(ctl);
+        }
+
+        self.remove_rule(or).ok()?;
+
+        None
     }
 
     fn sieve_outf2(&mut self, target_atoms: &[String]) -> super::Result<Vec<String>> {
